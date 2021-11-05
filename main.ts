5cc9d1860903f8a1933f6f5f5f506fd409418698
huskypel <92863057+huskypel@users.noreply.github.com>
2021-11-05 11:49:17 -0400

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -10,6 +10,11 @@ input.onButtonPressed(Button.B, function () {
 let nombre_de_personne = 0
 nombre_de_personne = 0
 basic.forever(function () {
+    if (0 > nombre_de_personne) {
+        nombre_de_personne = 0
+    }
+})
+basic.forever(function () {
     basic.showString("" + (nombre_de_personne))
     if (nombre_de_personne == 10) {
         basic.showString("attention!!!")
