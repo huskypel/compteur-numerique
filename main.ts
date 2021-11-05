b685a48bc0ec45782877470303aadb4434cf2235
huskypel <92863057+huskypel@users.noreply.github.com>
2021-11-05 11:24:52 -0400

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,24 @@
+input.onButtonPressed(Button.A, function () {
+    nombre_de_personne += 1
+})
+input.onButtonPressed(Button.AB, function () {
+    nombre_de_personne = 0
+})
+input.onButtonPressed(Button.B, function () {
+    nombre_de_personne += -1
+})
+let nombre_de_personne = 0
+nombre_de_personne = 0
+basic.forever(function () {
+    basic.showString("" + (nombre_de_personne))
+    if (nombre_de_personne == 10) {
+        basic.showString("attention!!!")
+        basic.showLeds(`
+            # . . . #
+            . # . # .
+            . . # . .
+            . # . # .
+            # . . . #
+            `)
+    }
+})
